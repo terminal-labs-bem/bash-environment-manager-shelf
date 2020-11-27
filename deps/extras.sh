d9866d7fa72f4908d3cf6c479028aa5068da3072
verhulstm <michael@terminallabs.com>
2020-11-26 20:19:58 -0600

    cleaning

--- deps/extras.sh
+++
@@ -1,8 +0,0 @@
-apt update -y --quiet
-apt upgrade -y --quiet
-
-apt install -y -f
-
-apt install -y --quiet ca-certificates
-
-DEBIAN_FRONTEND=noninteractive apt -y -q -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" full-upgrade
--- pypi/demo/demo/__init__.py
+++
@@ -1,3 +0,0 @@
-# Inside of __init__.py
-from demo.File0 import ClassA
-from demo.File1 import ClassB
