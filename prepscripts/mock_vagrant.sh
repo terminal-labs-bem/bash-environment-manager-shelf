e44b2b8328af64592989d8ea978aca4a21555a37
Michael Verhulst <michael@terminallabs.com>
2021-10-29 19:01:11 -0500

    Update mock_vagrant.sh

--- prepscripts/mock_vagrant.sh
+++ prepscripts/mock_vagrant.sh
@@ -1,10 +1,10 @@
-useradd -m vagrant
-mkdir -p /home/vagrant
-chown -R vagrant /home/vagrant
+adduser --disabled-password --gecos "" vagrant
 touch /home/vagrant/.bashrc
+usermod -aG sudo vagrant
 
 mkdir -p /vagrant
 cp -a . /vagrant/
+chmod 777 -R /vagrant
 chown -R vagrant /vagrant
 
 echo -e vagrant ALL=\(ALL\) NOPASSWD: ALL >> /etc/sudoers
