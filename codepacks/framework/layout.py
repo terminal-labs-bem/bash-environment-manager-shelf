4396e5e81945709f876d86d5ee1d7f88be0768bd
Michael Verhulst <michael@terminallabs.com>
2021-10-31 19:15:50 -0500

    Update layout.py

--- codepacks/framework/layout.py
+++ codepacks/framework/layout.py
@@ -1,7 +1,7 @@
 import os
 
 with open(os.path.dirname(__file__) + "/lib.py") as f:
-    code = compile(f.read(), "loader.py", "exec")
+    code = compile(f.read(), "lib.py", "exec")
     exec(code)
 
 _pgk_name = _get_pgk_name()
