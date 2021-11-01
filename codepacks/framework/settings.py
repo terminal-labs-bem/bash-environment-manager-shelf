6ac55498b8c379cf2d5f4dc94daa1c46d1ed810a
Michael Verhulst <michael@terminallabs.com>
2021-10-31 21:59:22 -0500

    Update settings.py

--- codepacks/framework/settings.py
+++ codepacks/framework/settings.py
@@ -1,73 +1,44 @@
 import os
+import site
+import tempfile
 import configparser
 
-with open(os.path.dirname(__file__) + "/lib.py") as f:
-    code = compile(f.read(), "lib.py", "exec")
+_lib = "lib.py"
+_shm = "/dev/shm"
+_tests = "tests"
+_back = ".."
+_blank = ""
+
+with open(os.path.dirname(__file__) + "/" + _lib) as f:
+    code = compile(f.read(), _lib, "exec")
+    exec(code)
+
+with open(_find_local_file()) as f:
+    code = compile(f.read(), "local.py", "exec")
     exec(code)
 
 config = configparser.ConfigParser()
-_pgk_name = _get_pgk_name()
-
-mod = f"{_pgk_name}.framework.derived_settings"
-PACKAGENAME = _import_fun(mod, "PACKAGENAME")
-PACKAGEDIR = _import_fun(mod, "PACKAGEDIR")
-FRAMEWORKDIR = _import_fun(mod, "FRAMEWORKDIR")
-SRCDIR = _import_fun(mod, "SRCDIR")
-APPDIR = _import_fun(mod, "APPDIR")
-SETUPFILEDIR = _import_fun(mod, "SETUPFILEDIR")
-TESTDIR = _import_fun(mod, "TESTDIR")
-MEMTEMPDIR = _import_fun(mod, "MEMTEMPDIR")
-SITEPACKAGESPATH = _import_fun(mod, "SITEPACKAGESPATH")
-
-mod = f"{_pgk_name}.framework.resolved_settings"
-get_env_variable = _import_fun(mod, "get_env_variable")
-resolve_payload_path = _import_fun(mod, "resolve_payload_path")
-
-config.read(PACKAGEDIR + "/setup.cfg")
+config.read(_find_config_file())
 
 VERSION = config["metadata"]["version"]
 NAME = config["metadata"]["name"]
 
-FRAMEWORK_VERSION = "0.0.1"
-PROJECT_NAME = NAME
-PRINT_VERBOSITY = "high"
-EXCLUDED_DIRS = [".DS_Store"]
-TEMPDIR = ".tmp/scratch"
-DIRS = [f"{TEMPDIR}"]
-TEXTTABLE_STYLE = ["-", "|", "+", "-"]
-MINIMUM_PYTHON_VERSION = (3, 6, 0)
+PACKAGENAME = NAME
+PACKAGEDIR = os.path.abspath(os.path.dirname(_find_config_file()))
+FRAMEWORKDIR = os.path.abspath(os.path.dirname(__file__))
+SRCDIR = os.path.abspath(os.path.join(FRAMEWORKDIR, _back))
+APPDIR = os.path.abspath(os.path.join(FRAMEWORKDIR, _back))
+SETUPFILEDIR = os.path.abspath(os.path.join(APPDIR, _back))
+TESTDIR = os.path.abspath(os.path.join(APPDIR, "tests"))
+if os.path.isdir(_shm):
+    MEMTEMPDIR = _shm
+    tempfile.tempdir = MEMTEMPDIR
+else:
+    MEMTEMPDIR = _blank
+SITEPACKAGESPATH = site.getsitepackages()[0]
 COVERAGERC_PATH = f"{APPDIR}/.coveragerc"
 
-# conda path
-# reponame = "code"
-# SETUP_NAME = reponame
-# EGG_NAME = SETUP_NAME.replace("_", "-")
-# PAYLOADPATH = SITEPACKAGESPATH  # noqa: F841
-# server_port = 5000
-# socket_host = "0.0.0.0"
-# PAYLOADPATH = resolve_payload_path(EGG_NAME, PROJECT_NAME)  # noqa: F821
-# POSTGRES_URL = get_env_variable("POSTGRES_URL")
-# POSTGRES_USER = get_env_variable("POSTGRES_USER")
-# POSTGRES_PW = get_env_variable("POSTGRES_PW")
-# POSTGRES_DB = get_env_variable("POSTGRES_DB")
-# DB_URL = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(
-#     user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL, db=POSTGRES_DB
-# )
-# MONGO_DB = PROJECT_NAME  # noqa: F821
-# UPLOAD_FOLDER = "uploads"
-# ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg", "gif", "zip"])
-# BASEDIR = os.path.abspath(os.path.dirname(__file__))
-# ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-# TEMPLATE_DIR = os.path.join(PAYLOADPATH, "templates")
-# STATIC_DIR = os.path.join(PAYLOADPATH, "static")
-# PERSISTENT_WORKING_DIRS = "stub"
-# CONFIG_DIC = {
-#     "POSTGRES_URL": POSTGRES_URL,
-#     "POSTGRES_USER": POSTGRES_USER,
-#     "POSTGRES_PW": POSTGRES_PW,
-#     "POSTGRES_DB": POSTGRES_DB,
-# }
-# tempfile.tempdir = TEMPDIR  # noqa: F821
+PROJECT_NAME = NAME
 
 VARS = {
     "VERSION": VERSION,
@@ -87,7 +58,6 @@ VARS = {
     "SRCDIR": SRCDIR,
     "APPDIR": APPDIR,
     "SETUPFILEDIR": SETUPFILEDIR,
-    "TESTDIR":  TESTDIR,
     "MEMTEMPDIR": MEMTEMPDIR,
     "SITEPACKAGESPATH": SITEPACKAGESPATH,
     "CONFIG": config._sections
