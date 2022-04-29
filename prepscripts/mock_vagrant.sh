37fa8b61fb6eea4cf947b3fb52dbc8216c400062
Michael Verhulst <michael@terminallabs.com>
2022-04-29 01:10:20 -0500

    Update mock_vagrant.sh

--- prepscripts/mock_vagrant.sh
+++ prepscripts/mock_vagrant.sh
@@ -12,7 +12,10 @@ chmod 777 -R /home/vagrant
 chown -R vagrant /home/vagrant
 
 mkdir -p /home/vagrant/.ssh
+ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -P ""
 chmod 700 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/id_rsa.pub
+chmod 600 /home/vagrant/.ssh/id_rsa
 chown -R vagrant /home/vagrant/.ssh
 
 echo -e vagrant ALL=\(ALL\) NOPASSWD: ALL >> /etc/sudoers
