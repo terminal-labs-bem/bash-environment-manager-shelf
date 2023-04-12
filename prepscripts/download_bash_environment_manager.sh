a8965a996024f93bee9bcc0da8b2ff31e6b51081
Michael Verhulst <michael@terminallabs.com>
2023-04-12 01:26:17 -0500

    Update download_bash_environment_manager.sh

--- prepscripts/download_bash_environment_manager.sh
+++ prepscripts/download_bash_environment_manager.sh
@@ -2,7 +2,8 @@ apt install -y wget unzip make build-essential
 su -m vagrant <<'EOF'
   cd /vagrant
   mkdir -p .tmp
-  cd .tmp
+  mkdir -p .tmp/wget
+  cd .tmp/wget
   wget -O bash-environment-manager.zip https://github.com/terminal-labs/bash-environment-manager/archive/master.zip  
   unzip bash-environment-manager.zip
 EOF
