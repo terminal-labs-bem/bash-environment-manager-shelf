3d843744bd47579b344a3ed3c5dcfa95ad59471b
Michael Verhulst <michael@terminallabs.com>
2023-07-10 04:05:12 -0500

    Update lib.sh

--- library/lib.sh
+++ library/lib.sh
@@ -1,5 +1,25 @@
 mkdir -p .tmp
 
+getmachine () {
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+      Linux*)     machine=Linux;;
+      Darwin*)    machine=Mac;;
+      CYGWIN*)    machine=Cygwin;;
+      MINGW*)     machine=MinGw;;
+      *)          machine="UNKNOWN:${unameOut}"
+  esac
+  MACHINE=$machine
+}
+
+getuserhome () {
+  if [[ $MACHINE == "Mac" ]]; then
+    USERHOME=/Users/$USERNAME
+  else
+    USERHOME=/home/$USERNAME
+  fi
+}
+
 emit_external_env_file () {
 cat > .tmp/_externalenv.sh <<EOF
 export BEM_DEBUG=-bemdebug-
@@ -18,6 +38,8 @@ export CMD=-cmd-
 export DPENAME=-dpename-
 export MACHINE=-machine-
 export WD=-wd-
+export PLUGIN=-plugin-
+export EXTRA=-extra-
 EOF
 }
 
@@ -39,56 +61,15 @@ export CMD=-cmd-
 export DPENAME=-dpename-
 export MACHINE=-machine-
 export WD=-wd-
+export PLUGIN=-plugin-
+export EXTRA=-extra-
 EOF
-}
-
-getmachine () {
-  unameOut="$(uname -s)"
-  case "${unameOut}" in
-      Linux*)     machine=Linux;;
-      Darwin*)    machine=Mac;;
-      CYGWIN*)    machine=Cygwin;;
-      MINGW*)     machine=MinGw;;
-      *)          machine="UNKNOWN:${unameOut}"
-  esac
-  MACHINE=$machine
-}
-
-getuserhome () {
-  if [[ $MACHINE == "Mac" ]]; then
-    USERHOME=/Users/$USERNAME
-  else
-    USERHOME=/home/$USERNAME
-  fi
-}
-
-clone_repo () {
-  mkdir -p .tmp/repos
-  cd .tmp/repos
-  git clone ${1}
-  cd ../..
-}
-
-install_project_repo_pip () {
-  mkdir -p .tmp/repos
-  cd .tmp/repos
-
-  path=${1}
-  fullname="${path##*/}"
-  dirname="${path%/*}"
-  basename="${fullname%.*}"
-  extension="${fullname##*.}"
-
-  cd ${basename}
-  pip install -e .
-  cd ../../..
 }
 
 edit_external_env_file () {
   sed "s#$1#\"$2\"#g" .tmp/_externalenv.sh > .tmp/_externalenv.tmp
   rm .tmp/_externalenv.sh
   mv .tmp/_externalenv.tmp .tmp/_externalenv.sh
-  cp .tmp/_externalenv.sh .tmp/_env.sh
 }
 
 edit_internal_env_file () {
