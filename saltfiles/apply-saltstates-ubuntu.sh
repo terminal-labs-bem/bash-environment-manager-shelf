5b80de69b8fe0199f653c3ce7334454564e6164c
Michael Verhulst <michael@terminallabs.com>
2023-12-10 02:17:17 -0600

    removed saltfiles

--- saltfiles/apply-saltstates-ubuntu.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install -y wget unzip
-wget https://raw.githubusercontent.com/terminal-labs/sample-states-installer/master/saltify-deploy-ubuntu.sh
-bash saltify-deploy-ubuntu.sh
-cd saltstack
-cp -r states/* /srv/salt
-cp grains/grains /etc/salt/grains
-salt-call --local state.highstate --log-level info
--- saltfiles/saltify-deploy-ubuntu.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install -y wget unzip
-wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
-bash bootstrap-salt.sh
-wget https://raw.githubusercontent.com/terminal-labs/sample-states-installer/master/deploy-ubuntu.sh
-sudo bash deploy-ubuntu.sh
