7c9a88f9fc891d19cdda8f8ac946929686d28b45
Michael Verhulst <michael@terminallabs.com>
2024-03-20 01:08:42 -0500

    Update mock_vagrant.sh

--- prepscripts/mock_vagrant.sh
+++ prepscripts/mock_vagrant.sh
@@ -2,7 +2,8 @@ apt remove --purge dotnet-sdk-* -y
 apt remove --purge dotnet-runtime-* -y
 apt-get purge --auto-remove dotnet* -y
 
-snap remove firefox
+apt-get purge --auto-remove ubuntu-advantage-tools -y
+apt-get purge --auto-remove ubuntu-pro-client-l10n -y
 
 adduser --disabled-password --gecos "" vagrant
 touch /home/vagrant/.bashrc
