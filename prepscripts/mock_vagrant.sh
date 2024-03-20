6fca7e2c3c4b8dabfc69f10a338bcf9583bade7d
Michael Verhulst <michael@terminallabs.com>
2024-03-20 02:09:52 -0500

    Update mock_vagrant.sh

--- prepscripts/mock_vagrant.sh
+++ prepscripts/mock_vagrant.sh
@@ -9,12 +9,9 @@ rm -rf /var/lib/update-notifier
 rm -rf /var/lib/ubuntu-release-upgrader
 apt-get purge --auto-remove ubuntu-advantage-tools -y
 
-apt remove firefox -y
-apt-get purge --auto-remove firefox -y
+apt-get purge --auto-remove firefox firefox* -y
 
-apt remove apache2 -y
-apt remove apache2* -y
-apt-get purge --auto-remove apache2 -y
+apt-get purge --auto-remove apache2 apache2* -y
 
 apt remove podman vim vim-common vim-runtime vim-tiny  -y
 apt remove vim* -y
