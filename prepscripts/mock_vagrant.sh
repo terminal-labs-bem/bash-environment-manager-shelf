38dc9c6deddc6197b96377bc6fdf399e53e46ef7
Michael Verhulst <michael@terminallabs.com>
2024-03-20 00:58:13 -0500

    Update mock_vagrant.sh

--- prepscripts/mock_vagrant.sh
+++ prepscripts/mock_vagrant.sh
@@ -1,3 +1,7 @@
+apt remove --purge dotnet-sdk-* -y
+apt remove --purge dotnet-runtime-* -y
+apt-get purge --auto-remove dotnet* -y
+
 adduser --disabled-password --gecos "" vagrant
 touch /home/vagrant/.bashrc
 usermod -aG sudo vagrant
