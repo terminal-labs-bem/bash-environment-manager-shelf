8fab07622160ca93b913dc6aa138068fd62101ec
Michael Verhulst <michael@terminallabs.com>
2024-04-21 03:55:26 -0500

    Update mock_vagrant.sh

--- prepfiles/mock_vagrant.sh
+++ prepfiles/mock_vagrant.sh
@@ -2,6 +2,11 @@ adduser --disabled-password --gecos "" vagrant
 touch /home/vagrant/.bashrc
 usermod -aG sudo vagrant
 
+mkdir -p /vagrant
 cp -a . /vagrant/
 chmod 777 -R /vagrant
 chown -R vagrant /vagrant
+
+mkdir -p /home/vagrant
+chmod 777 -R /home/vagrant
+chown -R vagrant /home/vagrant
